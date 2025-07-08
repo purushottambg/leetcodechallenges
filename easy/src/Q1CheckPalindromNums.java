048425ed657d094cfc585b5a8c2b65abc015c8a7
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-07-08 23:05:40 +0530

    Addded the Git program for palindrome check

---
+++ easy/src/Q1CheckPalindromNums.java
@@ -0,0 +1,20 @@
+public class Q1CheckPalindromNums {
+    public static void main(String[] args) {
+        int num=110011;
+        int original=num;
+        int reverse=0;
+        while(num>9){
+            reverse=reverse*10;
+            reverse=reverse+num%10;
+            num=num/10;
+        }
+        if (num>0 && num<=9){
+            reverse=reverse*10;
+            reverse+=num;
+        }
+        if (reverse==original)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}

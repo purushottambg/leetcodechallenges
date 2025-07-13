e4d07ff07893a88c8ce5edc3c71a0a71f3786a37
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-07-13 19:53:03 +0530

    Added the linked list issue fixed

--- easy/src/MergeTwoSortedList.java
+++ easy/src/MergeTwoSortedList.java
@@ -1,18 +1,17 @@
 import java.util.*;
 
 public class MergeTwoSortedList {
-    public static void main(String[] args) {
-        List<Integer> list1 = Arrays.asList(1, 2, 5, 7, 8, 9, 12, 54, 87, 97);
-        List<Integer> list2 = Arrays.asList(4, 6, 7, 11, 19, 21, 34, 57, 75, 88, 97);
-        List<Integer> finalList = new ArrayList<>();
-
-        finalList.addAll(list1);
-        finalList.addAll(list2);
-        Collections.sort(finalList);
-
-        for (Integer element: finalList){
-            System.out.print(element+"-> ");
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            if (list1 == null || list2 == null)
+                return list1 == null ? list2 : list1;
+            if (list1.val > list2.val) {
+                ListNode temp = list1;
+                list1 = list2;
+                list2 = temp;
+            }
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         }
-
     }
 }

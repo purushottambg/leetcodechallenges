5a112b611a5297b9e66883b5e1dd950c6106574e
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-07-20 23:31:19 +0530

    Added the program to check the square root

---
+++ other/src/SquareRoot69.java
@@ -0,0 +1,25 @@
+public class SquareRoot69 {
+    public static void main(String[] args) {
+        System.out.println("Square root for 123:"+squareRoot(123));
+    }
+    public static int squareRoot(int x){
+        if (x == 0 || x == 1)
+            return x;
+
+        int start = 1;
+        int end = x;
+        int mid = -1;
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+
+            if ((long) mid * mid > (long) x)
+                end = mid - 1;
+            else if (mid * mid == x)
+                return mid;
+            else
+                start = mid + 1;
+        }
+        return Math.round(end);
+    }
+}
